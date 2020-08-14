d0f3de39e929d0c3a776fabfa5ff3c1847cb2771
Cameron Harder-Hutton <cameronhh@iinet.net.au>
2020-08-14 23:55:37 +1000

    Update theme

--- src/site-config.js
+++ src/site-config.js
@@ -4,6 +4,16 @@ import { grommet } from "grommet"
 export const siteName = 'Checkomo';
 
 export const siteTheme = deepMerge(grommet, {
+  global: {
+    colors: {
+      brand: '#3D138D',
+      "neutral-2": '#7D4CDB',
+    },
+    // colors: {
+    //   brand: '#7D4CDB',
+    //   "neutral-2": '#3D138D',
+    // },
+  },
   formField: {
     error: {
       size: 'xsmall',
