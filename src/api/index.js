77be6169b4cf83a10c6b6b8f68115a07f3dfda57
Cameron Harder-Hutton <cameronhh@iinet.net.au>
2020-08-19 22:35:31 +1000

    Add client metadata dump

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,35 @@
 import axios from 'axios';
 import moment from 'moment';
 
+const getMetaData = () => ({
+  timezone: (new Date()).getTimezoneOffset() / 60,
+  pageon: window.location.pathname,
+  referrer: document.referrer,
+  previousSites: window.history.length,
+  browserName: navigator.appName,
+  browserEngine: navigator.product,
+  browserVersion1a: navigator.appVersion,
+  browserVersion1b: navigator.userAgent,
+  browserLanguage: navigator.language,
+  browserOnline: navigator.onLine,
+  browserPlatform: navigator.platform,
+  javaEnabled: navigator.javaEnabled(),
+  dataCookiesEnabled: navigator.cookieEnabled,
+  dataCookies1: document.cookie,
+  dataCookies2: decodeURIComponent(document.cookie.split(";")),
+  dataStorage: localStorage,
+  sizeScreenW: window.screen.width,
+  sizeScreenH: window.screen.height,
+  sizeDocW: document.width,
+  sizeDocH: document.height,
+  sizeInW: window.innerWidth,
+  sizeInH: window.innerHeight,
+  sizeAvailW: window.screen.availWidth,
+  sizeAvailH: window.screen.availHeight,
+  scrColorDepth: window.screen.colorDepth,
+  scrPixelDepth: window.screen.pixelDepth,
+});
+
 export async function getVenueDetail(venueId) {
   const res = await axios.get(`${process.env.REACT_APP_API_BASE}/venue/${venueId}`);
 
@@ -10,7 +39,7 @@ export async function getVenueDetail(venueId) {
 export async function getVenueCodeDetail(venueId, venueCode) {
   const res = await axios.get(`${process.env.REACT_APP_API_BASE}/venue/${venueId}/venuecode?code=${venueCode}`);
   const isValidCode = res.data.end_dttm ? moment.utc(res.data.end_dttm).isAfter(moment.utc()) : true
-  return { name: res.data.name, id: res.data.id, isValidCode: isValidCode};
+  return { name: res.data.name, id: res.data.id, isValidCode: isValidCode };
 }
 
 export const postVisit = async (mutationData) => {
@@ -24,8 +53,10 @@ export const postVisit = async (mutationData) => {
     "venue_code_id": mutationData.venueCodeId,
     "out_dttm": null,
     "clustered_id": null,
-    "meta_info": "",
+    "meta_info": JSON.stringify(getMetaData()),
   }
+
+  console.log(body)
 
   const res = await axios.post(
     `${process.env.REACT_APP_API_BASE}/venue/${mutationData.venueId}/visit?venue_code=${mutationData.venueCode}`,
